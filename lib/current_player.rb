496ed4985260e9556da14cadd54270be3a79b401
Alexei Lourie <alexeilourie@gmail.com>
2018-08-28 14:25:54 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -7,3 +7,12 @@ def turn_count(board)
   end
   return turn_count
 end
+
+def current_player(board)
+  turn_count = turn_count(board)
+  if turn_count % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
